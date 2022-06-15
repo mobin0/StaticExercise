8f2ad3c5b93b48ef8c2d7938978666a5e2c36033
Mobin Skaria <mobin0@berkeley.edu>
2022-06-15 10:22:54 -0700

    Static Exercise

---
+++ StaticExercise/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    static class TempConverter
+    {
+        public static double FarenheitToCelsius(double f){
+            return (f - 32) * 5.0 / 9.0;
+}
+        public static double CelsiusToFarenheit(double c) {
+            return (c * 9.0 / 5.0) + 32;
+        }
+    }
+}
---
+++ StaticExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Converting Farenheit to Celsius");
+            double FTemp = Convert.ToDouble(Console.ReadLine());
+            double convertToCelsius = TempConverter.FarenheitToCelsius(FTemp);
+            Console.WriteLine($"{FTemp} Farenheit is {convertToCelsius} Celsius");
+            Console.WriteLine($"Converting Celsius to Farenheit");
+            double CTemp = Convert.ToDouble(Console.ReadLine());
+            double convertToFarenheit = TempConverter.CelsiusToFarenheit(CTemp);
+            Console.WriteLine($"{CTemp} Celsius is {convertToFarenheit} Farenheit");
+          
+
+        }
+    }
+}
